16f95dfd4c11fe1a919128fae728607930baf563
marisleidids84 <marisleidids84@gmail.com>
2021-02-11 17:35:22 -0300

    feed modal terminado

--- src/Views/Feed.js
+++ src/Views/Feed.js
@@ -4,23 +4,66 @@ import '../App.css';
 
 function Feed() {
     return (
-        <div className="container">
-            <div className="row mt-5">
-                <div className="col-md-6 mt-5">
-                    <div className="play card border"></div>
-                </div>
-                <div className="col-md-6">
-                    <input className="imputfeed btn btn-rounded" type="button" value="¿Que estas pensando?"></input>
-                    <div className="row mb-2 mt-2">
-                        <CardFeed />
+        <>
+            <div className="container">
+                <div className="row mt-5">
+                    <div className="col-md col-sm">
+                        <div className="play card border"></div>
                     </div>
-                    <div className="row">
-                        <CardFeed />
-                    </div>
+                    <div className="col-md">
+                        <div className="card-header rounded">
+                            <div className="row">
+                                <div className="col-md col-sm">
+                                    <div className="fotoperfilfeed bg-success"></div>
+                                </div>
+                                <div className="col-md col-sm">
+                                    <input className="imputfeed btn btn-rounded" type="button" value="¿Que estas pensando?" data-toggle="modal" data-target="#exampleModal"></input>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row mb-2 mt-2">
+                            <div className="col-md col-sm">
+                                <CardFeed />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md col-sm">
+                                <CardFeed />
+                            </div>
+                        </div>
 
+                    </div>
+                </div>
+            </div >
+
+            {/* <!-- Modal --> */}
+            <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header bg-success">
+                            <h3 className="modal-title text-white" id="exampleModalLabel">Crear publicación</h3>
+                            <button type="button" className="close text-white" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            <div className="row">
+                                <div className="col-md-3 col-sm">
+                                    <img className="fotoperfilfeed bg-success rounded-circle" src="#" />
+                                </div>
+                                <div className="col-md-9 col-sm">
+                                    <h5>Marisleidis Perdomo</h5>
+                                </div>
+                            </div>
+                            <textarea className="w-100 border-0 mt-4" type="text" placeholder="¿Que estas pensando, Marisleidis?"></textarea>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-success">Post</button>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
