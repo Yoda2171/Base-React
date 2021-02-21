93e283826c3e49cac850b53f4aafabd02b01865a
JoshuaBravo20 <69777229+JoshuaBravo20@users.noreply.github.com>
2021-02-21 00:22:04 -0300

    paver

--- src/store/flux.js
+++ src/store/flux.js
@@ -212,7 +212,7 @@ const getState = ({ getStore, getActions, setStore }) => {
                                 setStore({
                                     artista: artist
                                 });
-                                fetch(`https://api.spotify.com/v1/artists/${store.artista.id}/top-tracks?country=US&`, {
+                                fetch(`https://api.spotify.com/v1/artists/${store.artista.id}/albums?market=US&limit=10`, {
                                     method: 'GET',
                                     headers: {
                                         'Content-Type': 'application/json',
@@ -222,7 +222,7 @@ const getState = ({ getStore, getActions, setStore }) => {
                                     .then(response => response.json())
                                     .then(data => {
                                         console.log('artista', data);
-                                        const tracks = data.tracks;
+                                        const tracks = data.items;
                                         setStore({
                                             tracks: tracks
                                         })
