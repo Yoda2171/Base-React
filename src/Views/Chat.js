4048d495cb22964d1ff626e7c94117e2f54ea0cb
marisleidids84 <marisleidids84@gmail.com>
2021-02-24 15:07:19 -0300

    mmm

--- src/Views/Chat.js
+++ src/Views/Chat.js
@@ -8,46 +8,22 @@ import Friends from "./Friends";
 import { io } from "socket.io-client";
 
 function Chat(props) {
-
+  
   const { store, actions } = useContext(Context);
 
   const history = useHistory();
+
 
   useEffect(() => {
     if (store.profile === null) history.push('/login');
   }, []);
 
-let users = store.userdb;
+
 
   return (
     <>
      
-      {
-        users.map((friends, i) => {
-          actions.getFollowId(friends.user_id);
-          console.log(store.followId[0])
-          if(store.followId[0]==true){
-            return (
-                <div key={i} className="row">
-                  <div className="col-4">
-                    <div className="list-group" id="list-tab" role="tablist">
-                      <a className="list-group-item list-group-item-action" id="list-home-list" data-toggle="list" href={'#'+friends.user_id} role="tab" aria-controls="home">{friends.name}</a>
-                    </div>
-                  </div>
-                  <div className="col-8">
-                    <div className="tab-content" id="nav-tabContent">
-                      <div className="tab-pane fade show active" id={friends.user_id} role="tabpanel" aria-labelledby="list-home-list"><RoomChat/></div>
-                    </div>
-                  </div>
-                </div>
-            )
-          }
-          else{
-            console.log("Este usuario no es tu amigo")
-          }
-        })
-      }
-
+      
     </>
   );
 }
